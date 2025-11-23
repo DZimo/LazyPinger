88f389f3a3b4a6a8c561aef65ff4a8d4e9e89b83
DZimo <53861899+DZimo@users.noreply.github.com>
2025-11-23 11:13:19 +0100

    Add of lazylogger

---
+++ LazyPinger.Base/Common/LazyLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyPinger.Base.Common
+{
+    public class LazyLogger
+    {
+        public static void LogAll(string msg, LogSeverity severity)
+        {
+            Console.WriteLine($"{DateTime.Now}:{severity}:{msg}");
+        }
+    }
+
+    public enum LogSeverity
+    {
+        Info, 
+        Warning, 
+        Error
+    }
+}
