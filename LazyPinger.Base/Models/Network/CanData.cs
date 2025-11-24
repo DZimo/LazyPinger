de88609e5e490b93b00e5bd160b62f018d7412d9
DZimo <53861899+DZimo@users.noreply.github.com>
2025-11-24 19:19:01 +0100

    Add of can models

---
+++ LazyPinger.Base/Models/Network/CanData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyPinger.Base.Models.Network
+{
+    internal class CanData
+    {
+    }
+}
---
+++ LazyPinger.Base/Models/Network/CanFrame.cs
@@ -0,0 +1,11 @@
+﻿namespace LazyPinger.Base.Models.Network
+{
+    public class CanFrame
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+    }
+}
